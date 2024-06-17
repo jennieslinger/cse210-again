b41e216eca79b5741deca8ac4d95f31b217485a2
jennieslinger <jennieslinger@gmail.com>
2024-06-17 15:14:38 -0700

    02 Learning Activity (Resume)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1989;
+        job1._endYear = 2013;
+        //job1.DisplayJob();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Analyst";
+        job2._startYear = 2014;
+        job2._endYear = 2023;
+        //job2.DisplayJob();
+
+        Resume myResume = new Resume();
+        myResume._name = "Kelley Marx";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
